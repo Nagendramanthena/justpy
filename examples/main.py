2714d229eef177632f385d76b72b62571a8568fa
nagendravarma <manthenanagendra@gmail.com>
2022-07-17 15:40:03 +0530

    added eventlisteners using functions and enabled caluclate button to work

--- examples/main.py
+++ examples/main.py
@@ -15,11 +15,23 @@ def home():
     jp.Div(text="Yet another div",a=div1,classes="text-gray-600")
 
     div2 = jp.Div(a=div,classes="grid grid-cols-2 gap-4 p-4")
-    # jp.Button(a=div2,text="Calculate",click=sumup,in1 = in_1,in2 = in_2,d1=d,classes="border border-blue-500 "
-                                            # "m-2 py-1 px-4 rounded text-blue-600 hover:bg-red-500 hover:text-white") #if you want to split leave space before oherwise we will not get the results
+    jp.Button(a=div2,text="Calculate",click=sumup,in1 = in_1,in2 = in_2,d1=d,classes="border border-blue-500 "
+                                             "m-2 py-1 px-4 rounded text-blue-600 hover:bg-red-500 hover:text-white") #if you want to split leave space before oherwise we will not get the results
 
-    # jp.Div(a=div2,text="cool interactive div",mouseenter = mouse_entered,mouseleave = mouse_left, classes="hover:bg-green-500 text-white bg-red-500  border border-gray-300 m-2 p-4")
+    jp.Div(a=div2,text="cool interactive div",mouseenter = mouse_entered,mouseleave = mouse_left, classes="hover:bg-green-500 text-white bg-red-500  border border-gray-300 m-2 p-4")
     return web
 
 
+def sumup(widget,msg):
+    sum = float(widget.in1.value)/float(widget.in2.value)
+    widget.d1.text = sum
+
+def mouse_entered(widget,msg):
+    print("hello")
+    widget.text = "mouse entered the house"
+
+def mouse_left(widget ,msg):
+    widget.text = "mouse left the house"
+jp.Route("/",home)
+jp.justpy()
 
