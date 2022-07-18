63d9c6bc5d65e76579b91883ec35b66c30d4ebe7
nagendravarma <manthenanagendra@gmail.com>
2022-07-18 11:35:18 +0530

    added Navbar into the Home page

--- webapp/home.py
+++ webapp/home.py
@@ -6,7 +6,16 @@ class Home:
     @classmethod
     def serve(cls,req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="leftDrawerOpen", side="left", bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat="True", round=True, icon="menu",click = cls.move_drawer,drawer=drawer)
+        toolbartitle = jp.QToolbarTitle(a=toolbar)
+        jp.QAvatar(a=toolbartitle,src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg")
+        Qpagecontainer = jp.QPageContainer(a=layout)
+        div = jp.Div(a=Qpagecontainer, classes="bg-gray-200 h-screen p-3")
         jp.Div(a=div, text="This is the Home page", classes="text-4xl m-2")
         jp.Div(a=div, text="""
                 jxdjsj nj j nnxnxnxn
@@ -15,6 +24,13 @@ class Home:
         jp.A(text='Search a word here', href="/dictionary", a=div,
              classes='m-2 p-2 text-xl text-white bg-blue-500 hover:bg-blue-700 ')
         return wp
+
+    @staticmethod
+    def move_drawer(widget,msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
 
 
 
